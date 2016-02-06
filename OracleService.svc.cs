950ac3af5829cc7fb3459f89287527e2f295e121
akois <akoislight@gmail.com>
2016-02-06 13:09:59 +0200

    Initial submit

---
+++ OracleService.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace OracleService
+{
+    [ServiceContract]
+    public class OracleService
+    {
+        [WebGet(UriTemplate = "Decrypt?cipher={cipher}")]
+        [OperationContract]
+        public string Decrypt(string cipher)
+        {
+            //throw new WebFaultException(HttpStatusCode.BadRequest);
+
+            return "cipher";
+           
+        }
+    }
+}
